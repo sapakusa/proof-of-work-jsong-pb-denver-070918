304c892a09ee5dc5b81dac0afd04b872b505a1cd
adamjonas <jonas@flatironschool.com>
2018-06-27 16:57:59 -0400

    Initial Commit

---
+++ test/index_test.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from ipynb.fs.full.index import *
+
+class BlockTest(TestCase):
+
+    def test_check_pow(self):
+        block_raw = bytes.fromhex('04000000fbedbbf0cfdaf278c094f187f2eb987c86a199da22bbb20400000000000000007b7697b29129648fa08b4bcd13c9d5e60abb973a1efac9c8d573c71c807c56c3d6213557faa80518c3737ec1')
+        stream = BytesIO(block_raw)
+        block = Block.parse(stream)
+        self.assertTrue(block.check_pow())
+        block_raw = bytes.fromhex('04000000fbedbbf0cfdaf278c094f187f2eb987c86a199da22bbb20400000000000000007b7697b29129648fa08b4bcd13c9d5e60abb973a1efac9c8d573c71c807c56c3d6213557faa80518c3737ec0')
+        stream = BytesIO(block_raw)
+        block = Block.parse(stream)
+        self.assertFalse(block.check_pow())
